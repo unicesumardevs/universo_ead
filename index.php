3f3608b6a26ffd5a3d07bb671e9060498469d228
unicesumardevs <namuser.email>
2022-08-22 16:26:07 -0300

    Correção na estrurura

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php include "inc/header.php"?>
-<?php include "main.php"?>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <h1 class="text-center">Conteúdo</h1>
+            </div>
+        </div>
+    </div>
+</div>
 <?php include "inc/footer.php"?>
